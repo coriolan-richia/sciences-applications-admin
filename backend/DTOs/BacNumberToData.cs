253d5893beecf7e74e71a216b3787356ca53c413
Sandaadrien <sandaniainaadrien@gmail.com>
2025-10-12 21:25:33 +0300

    the doesBacExist method added

---
+++ backend/DTOs/BacNumberToData.cs
@@ -0,0 +1,8 @@
+namespace backend.DTOs
+{
+    public class BacNumberToData
+    {
+        public string NumBacc { get; set; } = string.Empty;
+        public string AnneeBacc { get; set; } = string.Empty;
+    }
+}
