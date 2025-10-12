7d76a65b626c12cdf554832fa963d8ac7ac7f62a
Sandaadrien <sandaniainaadrien@gmail.com>
2025-10-12 09:31:55 +0300

    the Listing all action finished

--- backend/Controllers/PreinscriptionController.cs
+++ backend/Controllers/PreinscriptionController.cs
@@ -1,4 +1,6 @@
 using backend.Context;
+using backend.DTOs;
+using backend.Models.Bac;
 using backend.Models.Fac;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,16 +12,42 @@ namespace backend.Controllers
     public class PreinscriptionController : ControllerBase
     {
         private readonly FacContext _facDBContext;
-        public PreinscriptionController(FacContext facDBContext)
+        private readonly BacContext _bacDBContext;
+        public PreinscriptionController(FacContext facDBContext,BacContext bacDBContext)
         {
             _facDBContext = facDBContext;
+            _bacDBContext = bacDBContext;
         }
         [HttpGet("listingall")]
         public async Task<IActionResult> ListingAll()
         {
             List<Preinscription> preinscriptions = await _facDBContext.Preinscriptions.ToListAsync();
+            List<Bachelier> bacheliers = await _bacDBContext.Bacheliers.ToListAsync();
+            List<Option> options = await _bacDBContext.Options.ToListAsync();
+            List<Parcour> parcours = await _facDBContext.Parcours.ToListAsync();
 
-            return Ok(preinscriptions);
+            var result = (from p in preinscriptions 
+                            join b in bacheliers on p.IdBac equals b.IdBachelier
+                            join o in options on b.IdOption equals o.IdOption
+                            join pr in parcours on p.IdPortail equals pr.IdParcours
+                            select new ListingModel
+                            {
+                                Id = p.IdPreinscription.ToString(),
+                                BacNumber = b.NumeroCandidat,
+                                BacYear = b.Annee.Year,
+                                BacOption = o.Serie,
+                                StudyBranch = pr.NomParcours ?? "No Parcour",
+                                PreregistrationDate = p.DatePreinscription?.ToString() ?? "No Preregistration date",
+                                Email = p.Email ?? "No Email Adress",
+                                Phone = p.Tel ?? "No Phone Number",
+                                PaymentDate = p.DatePaiement.ToString() ?? "No Payment date",
+                                PaymentReference = p.RefBancaire,
+                                PaymentAgence = p.Agence,
+                                Status = p.EstValide ?? false ? "verified" : "pending"
+                            }
+            ).ToList();
+
+            return Ok(result);
         }
         
     }
--- backend/DTOs/ListingModel.cs
+++ backend/DTOs/ListingModel.cs
@@ -8,13 +8,13 @@ namespace backend.DTOs
         }
         public string Id { get; set; } = string.Empty;
         public string BacNumber { get; set; } = string.Empty;
-        public DateOnly BacYear { get; set; }
+        public int BacYear { get; set; }
         public string BacOption { get; set; } = string.Empty;
         public string StudyBranch { get; set; } = string.Empty;
-        public DateTime PreregistrationDate { get; set; }
+        public string PreregistrationDate { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
-        public DateTime PaymentDate { get; set; }
+        public string PaymentDate { get; set; } = string.Empty;
         public string PaymentReference { get; set; } = string.Empty;
         public string PaymentAgence { get; set; } = string.Empty;
         public string Status { get; set; } = string.Empty;
