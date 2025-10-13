700a5f208beee0b155387dce64d0fde04d11c06a
Coriolan Richia <andriamiharinjaracoriolanr.acr@gmail.com>
2025-10-13 14:44:10 +0300

    User Listing API

---
+++ backend/Controllers/UtilisateurController.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using backend.Context;
+using backend.Models.Fac;
+using backend.Services;
+using backend.DTOs;
+using System.Linq;
+
+namespace MyApp.Namespace
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UtilisateurController(FacContext facDBContext, IPasswordHasher<Utilisateur> hasher, JwtService jwtService): ControllerBase
+    {
+        private readonly FacContext _facDBContext = facDBContext;
+        private readonly IPasswordHasher<Utilisateur> _hasher = hasher;
+        private readonly JwtService _jwtService = jwtService;
+
+        [HttpGet("list-users")]
+        public async Task<IActionResult> ListUsers(int id)
+        {
+            var authUser = _facDBContext.Utilisateurs
+                .Include(u => u.RoleUtilisateurs)
+                .ThenInclude(ru => ru.IdRoleNavigation)
+                .FirstOrDefault(u => u.IdUtilisateur == id);
+            
+            if(authUser is null)
+            {
+                return BadRequest(new { error="Votre utilisateur authentifié n'est pas inscrit sur le serveur." });
+            }
+
+            int? idRole = authUser?.RoleUtilisateurs.FirstOrDefault()?.IdRole;
+            if(idRole!=1)
+            {
+                return StatusCode(401, new { error = "Vous n'êtes pas autorisé à accéder à cette ressource;"});
+                
+            }
+
+            List<UserForListing> users = await (from us in _facDBContext.Utilisateurs
+                join ru in _facDBContext.RoleUtilisateurs on us.IdUtilisateur equals ru.IdUtilisateur
+                join ro in _facDBContext.Roles on ru.IdRole equals ro.IdRole
+                group new { ru, ro } by new { us.IdUtilisateur, us.Identifiant } into g
+                select new UserForListing
+                {
+                    IdUtilisateur = g.Key.IdUtilisateur,
+                    Identifiant = g.Key.Identifiant,
+                    Role = g.Select(x => x.ro.NomRole).FirstOrDefault()
+                }).ToListAsync();
+            
+            
+
+            return Ok(users);
+        }
+        
+    }
+}
